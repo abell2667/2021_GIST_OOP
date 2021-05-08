a712d9c64e1dd55e6126bf50a8b828693cc44f9e
abell2667 <abell2667@gm.gist.ac.kr>
2021-05-08 22:46:53 +0900

    HW_01 : Stack.fixtypo

--- src/gist/oop/hw/hw01/Stack.java
+++ src/gist/oop/hw/hw01/Stack.java
@@ -7,30 +7,30 @@ package gist.oop.hw.hw01;
 
 class Stack {
 
-	private char s[] ; // 스택 문자 데이터를 담고있는 배열
+	private char data[] ; // 스택 문자 데이터를 담고있는 배열
 
-	private int toploc ; // 삽입과 삭제가 발생하는 스택 top을 가르킴. 즉, 저장된 문자
+	private int top; // 삽입과 삭제가 발생하는 스택 top을 가르킴. 즉, 저장된 문자
 						 // 데이터의 개 수 정보를 갖는 변수
 	
 	// 1. 주어진 크기의 빈 스택을 만드는 생성자
 	Stack(int size) {
-		s = new char[size];	// 스택 저장공간을 할당
-		toploc = 0;			// 스택 top을 초기화
+		data = new char[size];	// 스택 저장공간을 할당
+		top = 0;			// 스택 top을 초기화
 	}
 
 	// 2. 주어진 스택과 동일한 스택을 만드는 생성자
 	Stack(Stack ob) {
-		toploc = ob.toploc; // 스택 top을 일치시킴
-		s = new char[ob.s.length]; // 스택 저장공간 할당 
+		top = ob.top; // 스택 top을 일치시킴
+		data = new char[ob.data.length]; // 스택 저장공간 할당 
 		// 스택 배열 element들을 복사
-		for(int i = 0; i < ob.s.length; i++)
-			s[i] = ob.s[i];	
+		for(int i = 0; i < ob.data.length; i++)
+			data[i] = ob.data[i];	
 	}
 
 	// 3. 주어진 문자 배열로 스택을 만드는 생성자
 	Stack(char a[]) {
-		toploc = 0 ;			// 스택 top을 초기화
-		s = new char[a.length];	// 스택 저장공간 할당
+		top = 0 ;			// 스택 top을 초기화
+		data = new char[a.length];	// 스택 저장공간 할당
 		// 스택 배열 element들을 복사
 		for(int i = 0; i < a.length; i++)
 			push(a[i]);
@@ -38,22 +38,22 @@ class Stack {
 	
 	// 주어진 문자를 스택에 삽입하는 메소드
 	void push(char ch) {
-		if (toploc == s.length) { // 스택이 가득차 있는지 check
+		if (top == data.length) { // 스택이 가득차 있는지 check
 			System.out.println("\n- 스택이 가득차 있음 -");
 			return;
 		}
 
-		s[toploc++] = ch ; // 스택 top에 문자를 복사하고 top을 증가시킴
+		data[top++] = ch ; // 스택 top에 문자를 복사하고 top을 증가시킴
 	}
 
 	// 스택의 top으로부터 문자를 읽어오는 메소드
 	char pop() {
-		if (toploc == 0) { //스택이 비어 있는지check
+		if (top == 0) { //스택이 비어 있는지check
 			System.out.println("\n- 스택이 비어 있음 -");
 			return (char)0;
 		}
 
-		return s[--toploc] ; // 스택 top에서 문자를 return시키고 top을 감소시킴 }
+		return data[--top] ; // 스택 top에서 문자를 return시키고 top을 감소시킴 }
 	}
 
 }
